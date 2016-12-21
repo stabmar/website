88afe377e31b7fec5e7d2bf2b9fde2eda5f6dfc1
MARCEL-ACER <staber.marcel@gmail.com>
2016-12-21 14:49:17 +0100

    -

--- TechfinIT/App_Start/BundleConfig.cs
+++ TechfinIT/App_Start/BundleConfig.cs
@@ -21,6 +21,7 @@ namespace TechfinIT
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/customjs.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
