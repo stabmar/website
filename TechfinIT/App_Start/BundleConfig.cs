ed30eeb842fe3c5fe28c369cc180664b8b0f656e
MARCEL-ACER <staber.marcel@gmail.com>
2016-12-21 17:15:39 +0100

    Renamed customjs to Productjs; Additional attempts to implement toggle for product page

--- TechfinIT/App_Start/BundleConfig.cs
+++ TechfinIT/App_Start/BundleConfig.cs
@@ -21,7 +21,7 @@ namespace TechfinIT
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/customjs.js",
+                      "~/Scripts/Productjs.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
---
+++ TechfinIT/Scripts/Productjs.js
@@ -0,0 +1,25 @@
+﻿// This Javascript is ment to define some additional js methods.
+
+$(document).ready(function () {
+    //Event-Handler: Changes display value for product description
+    $('button.productModule').click(function () {
+        var e = $(this).next('div');
+        if (e.css('style.display') === 'none') {
+            e.css('style.display','block');
+        }
+        else {
+            e.css('style.display','none');
+        }
+    })
+})
+
+// Aternative function fo handle display values
+function changeVisibility(id) {
+    var e = document.getElementById(id);
+    if ($(e).css(style.display) === "none") {
+        $(e).css(style.display) = "block";
+    }
+    else {
+        $(e).css(style.display) = "none";
+    }
+}
