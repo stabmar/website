68c28ff629efc8646a4e882870b77156b3f65583
MARCEL-ACER <staber.marcel@gmail.com>
2016-12-22 11:38:39 +0100

    Started refactoring of Scripts/Product.js

--- TechfinIT/Scripts/Productjs.js
+++ TechfinIT/Scripts/Productjs.js
@@ -1,41 +1,7 @@
 ﻿// This Javascript is ment to define some additional js methods.
-$(document).unload
-/*
 $(document).ready(function () {
-    
+    alert('Dokument ist fertig geladen');
     $('.productModuleButton').click(function () {
-        $('.moduleDescription').show();
+        $('.moduleDescription').toggle('slow');
     })
 })
-*/
-
-$(document).ready(function () {
-    document.addEventListener("click", function () {
-        document.getElementById('1').hide();
-    })
-})
-
-/*
-$(document).ready(function () {
-    //Event-Handler: Changes display value for product description
-    $('button.productModule').click(function () {
-        var e = $(this).next('div');
-        if (e.css('style.display') === 'none') {
-            e.css('style.display','block');
-        }
-        else {
-            e.css('style.display','none');
-        }
-    })
-})
-
-// Aternative function fo handle display values
-function changeVisibility(id) {
-    var e = document.getElementById(id);
-    if ($(e).css(style.display) === "none") {
-        $(e).css(style.display) = "block";
-    }
-    else {
-        $(e).css(style.display) = "none";
-    }
-}*/
