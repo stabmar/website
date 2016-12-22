7acb169cc2ae9fb3d79c5686ec38fdb84587a71b
MARCEL-ACER <staber.marcel@gmail.com>
2016-12-22 13:12:13 +0100

    started integration of bootstrap buttons and containers

--- TechfinIT/Scripts/Productjs.js
+++ TechfinIT/Scripts/Productjs.js
@@ -1,7 +1,7 @@
 ﻿// This Javascript is ment to define some additional js methods.
 $(document).ready(function () {
-    alert('Dokument ist fertig geladen');
-    $('.productModuleButton').click(function () {
-        $(this).next('.moduleDescription').toggle('slow');
+    //alert('Dokument ist fertig geladen');
+    $('button').click(function () {
+        $(this).next('#moduleDescription').toggle('slow');
     })
 })
